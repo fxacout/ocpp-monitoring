f9c02dbcfc4101809af999cfede4de1e52be1072
fxacout <fastcobos@gmail.com>
2023-07-26 19:36:28 +0200

    Wip. Improved front

---
+++ http/ocpp-monitoring-ui/src/pages/chargepoints.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Audit } from "@/components/Audit";
+import { CustomMap } from "@/components/Map";
+import { NavBar } from "@/components/NavBar";
+import { NodeStatus } from "@/components/NodeStatus";
+import { useSession } from "next-auth/react";
+import { useRouter } from 'next/router';
+import { Col, Container, Row } from 'react-bootstrap';
+import { PageSkeleton } from '../components/PageSkeleton';
+
+export default function Index() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  return (
+    <PageSkeleton>
+      <Container fluid>
+        <Row>
+        <Col>
+          <CustomMap></CustomMap>
+        </Col>
+        <Col>
+          <Row>
+            <NodeStatus></NodeStatus>
+            </Row>
+            <Row>
+            <Audit></Audit>
+            </Row>
+          
+        </Col>
+        </Row>
+      </Container>
+      </PageSkeleton>
+  );
+}
